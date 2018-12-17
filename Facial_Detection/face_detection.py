87e19888647650aed9c049ccda8c0eaea0d33c92
mephi009 <roysumit949@gmail.com>
2018-12-17 11:37:31 +0530

    Successful to get data of the face movement

---
+++ Facial_Detection/face_detection.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import cv2
+
+
+face_cas = cv2.CascadeClassifier('/home/sumit/imsge_processing/venv/include/Facial_Detection/data/haarcascades/haarcascade_frontalface_alt.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cas.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors= 5)
+    cv2.imshow('frame', frame)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
