f7aac9da08af6731332dc2c2a2128e3683368b68
mephi009 <roysumit949@gmail.com>
2018-12-19 09:22:09 +0530

    storing image pixels as an numpy array

--- Facial_Detection/face-train.py
+++ Facial_Detection/face-train.py
@@ -1,10 +1,32 @@
 import os
+import numpy as np
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "Images")
+x_train =[]
+y_labels=[]
 
 for root,dirs, files in os.walk(image_dir):
     for file in files:
         if file.endswith("png") or file.endswith("jpg"):
             path = os.path.join(root,file)
-            print(path)
+            label = os.path.basename(os.path.dirname(path)).replace(" ","_").lower()
+            print(label,path)
+            # x_train.append(path)
+            # y_labels.append(path)
+            pil_image = Image.open(path).convert("L") #grauscale
+            image_array =np.array(pil_image, "uint8") #"uint8 = unsigned integer of 8 bite
+            print(image_array)
+
+
+
+
+
+
+
+
+
+
+
+
