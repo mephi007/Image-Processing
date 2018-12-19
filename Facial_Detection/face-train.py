1a21c4359b8c2ea8c7ac81a2840dca6d047ed7b6
mephi009 <roysumit949@gmail.com>
2018-12-19 09:08:02 +0530

    python file to train for image recognizer

---
+++ Facial_Detection/face-train.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "Images")
+
+for root,dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root,file)
+            print(path)
